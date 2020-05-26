69c733a10e8c92a9b90f4943d9fd152dea654c17
Ola Embreus <embreus@chalmers.se>
2020-05-26 15:23:20 +0200

    fixed p=0 collision frequency bugs

--- include/DREAM/Equations/PitchScatterFrequency.hpp
+++ include/DREAM/Equations/PitchScatterFrequency.hpp
@@ -38,7 +38,7 @@ namespace DREAM {
         virtual real_t evaluateElectronTermAtP(len_t ir, real_t p) override;
         virtual real_t evaluateScreenedTermAtP(len_t iz, len_t Z0, real_t p) override;
         virtual real_t evaluateIonTermAtP(len_t iz, len_t Z0, real_t p) override;
-        virtual real_t evaluatePreFactorAtP(real_t p) override {return constPreFactor * sqrt(1+p*p)/(p*p*p);}
+        virtual real_t evaluatePreFactorAtP(real_t p) override {if(p==0) return 0; else return constPreFactor * sqrt(1+p*p)/(p*p*p);}
    protected:
         virtual real_t GetAtomicParameter(len_t iz, len_t Z0) override;
 
