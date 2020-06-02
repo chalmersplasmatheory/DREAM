c517980eb6f85b0e1dfbc704031d8ecb18d43e98
Ola Embreus <embreus@chalmers.se>
2020-06-02 20:00:27 +0200

    Implementation of class that calculates current density j_par/(B/Bmin) from f

---
+++ include/DREAM/Equations/Fluid/CurrentDensityFromDistributionFunction.hpp
@@ -0,0 +1,21 @@
+#ifndef _DREAM_EQUATION_FLUID_CURRENT_DENSITY_FROM_DISTRIBUTION_FUNCTION_HPP
+#define _DREAM_EQUATION_FLUID_CURRENT_DENSITY_FROM_DISTRIBUTION_FUNCTION_HPP
+
+#include "FVM/Equation/MomentQuantity.hpp"
+#include "FVM/Grid/Grid.hpp"
+#include "FVM/UnknownQuantityHandler.hpp"
+#include "DREAM/Constants.hpp"
+
+namespace DREAM {
+    class CurrentDensityFromDistributionFunction : public FVM::MomentQuantity {
+    private:
+    public:
+        CurrentDensityFromDistributionFunction(FVM::Grid*, FVM::Grid*, len_t, len_t);
+        virtual ~CurrentDensityFromDistributionFunction();
+
+        virtual bool GridRebuilt() override;
+        virtual void Rebuild(const real_t, const real_t, FVM::UnknownQuantityHandler*) override {}
+    };
+}
+
+#endif/*_DREAM_EQUATION_FLUID_CURRENT_DENSITY_FROM_DISTRIBUTION_FUNCTION_HPP*/
---
+++ src/Equations/Fluid/CurrentDensityFromDistributionFunction.cpp
@@ -0,0 +1,70 @@
+/**
+ * Implementation of an operator which evaluates the parallel current density
+ * moment j_||/(B/Bmin) normalized to the local magnetic field strength
+ * of a given distribution function.
+ */
+
+#include "DREAM/Equations/Fluid/CurrentDensityFromDistributionFunction.hpp"
+#include "FVM/Grid/Grid.hpp"
+#include "FVM/Grid/MomentumGrid.hpp"
+#include "FVM/UnknownQuantityHandler.hpp"
+
+using namespace DREAM;
+
+
+/**
+ * Constructor.
+ */
+CurrentDensityFromDistributionFunction::CurrentDensityFromDistributionFunction(
+    FVM::Grid *densityGrid, FVM::Grid *distributionGrid, len_t id_n, len_t id_f
+) : MomentQuantity(densityGrid, distributionGrid, id_n, id_f) {
+    
+    // Build moment integrand
+    this->GridRebuilt();
+}
+
+
+/**
+ * Destructor.
+ */
+CurrentDensityFromDistributionFunction::~CurrentDensityFromDistributionFunction() { }
+
+
+/**
+ * Method that is called whenever the grid is rebuilt. We only
+ * need to rebuild this EquationTerm if the total number of grid
+ * cells changes.
+ */
+bool CurrentDensityFromDistributionFunction::GridRebuilt() {
+    if (this->MomentQuantity::GridRebuilt()) {
+        // XXX assumes same momentumgrid at all radii.
+        FVM::MomentumGrid *mg;
+        FVM::RadialGrid *rGrid = fGrid->GetRadialGrid();
+
+        len_t np1, np2, ind;
+        real_t v, xi0, geometricFactor;
+        real_t *const*bounceAverage = rGrid->GetBA_xiOverBR2();
+        const real_t *fluxSurfaceAverage = rGrid->GetFSA_1OverR2();
+        //for (len_t i = 0; i < this->nIntegrand; i++){ // i = j0*np1 + i0. j0 = i/np1. i0 = i%np1 
+        for(len_t ir = 0; ir<rGrid->GetNr(); ir++){
+            mg = fGrid->GetMomentumGrid(ir);
+            np1 = mg->GetNp1();
+            np2 = mg->GetNp2();
+            for(len_t ip1 = 0; ip1<np1; ip1++){
+                for(len_t ip2 = 0; ip2<np2; ip2++){
+                    ind = ir*np1*np2+ip2*np1+ip1;
+                    v = Constants::c *mg->GetP(ip1,ip2)/mg->GetGamma(ip1,ip2);
+                    if(rGrid->GetIsTrapped(mg,ir,ip1,ip2,FVM::FLUXGRIDTYPE_DISTRIBUTION))
+                        this->integrand[ind] = 0;
+                    else {
+                        xi0 = mg->GetXi0(ip1,ip2);
+                        geometricFactor = bounceAverage[ir][ip2*np1+ip1] / fluxSurfaceAverage[ir];
+                        this->integrand[ind] = Constants::ec * v * xi0 * geometricFactor;
+                    }
+                }
+            }
+        }
+
+        return true;
+    } else return false;
+}
