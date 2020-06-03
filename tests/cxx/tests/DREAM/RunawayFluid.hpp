58636a544290f10f59cd60851651be6d9dc3d556
Ola Embreus <embreus@chalmers.se>
2020-06-03 15:00:05 +0200

    Implemented RunawayFluid test, with Eceff calculation.

---
+++ tests/cxx/tests/DREAM/RunawayFluid.hpp
@@ -0,0 +1,30 @@
+#ifndef _DREAMTESTS_DREAM_RUNAWAY_FLUID_HPP
+#define _DREAMTESTS_DREAM_RUNAWAY_FLUID_HPP
+
+#include <string>
+#include "FVM/Grid/Grid.hpp"
+#include "FVM/UnknownQuantityHandler.hpp"
+#include "DREAM/IonHandler.hpp"
+#include "DREAM/Equations/RunawayFluid.hpp"
+#include "UnitTest.hpp"
+
+namespace DREAMTESTS::_DREAM {
+    class RunawayFluid : public UnitTest {
+    private:
+        len_t id_ions;
+
+    public:
+        RunawayFluid(const std::string& s) : UnitTest(s) {}
+
+        DREAM::IonHandler *GetIonHandler(DREAM::FVM::Grid*, DREAM::FVM::UnknownQuantityHandler*, const len_t, const len_t*);
+        DREAM::FVM::UnknownQuantityHandler *GetUnknownHandler(DREAM::FVM::Grid*,
+            const len_t, const len_t*, const real_t, const real_t);
+        DREAM::RunawayFluid *GetRunawayFluid(DREAM::CollisionQuantity::collqty_settings *cq, const len_t, const len_t*,const real_t, const real_t, const real_t);
+        bool CompareEceffWithTabulated();
+
+//        bool CheckConservativity();
+        virtual bool Run(bool) override;
+    };
+}
+
+#endif/*_DREAMTESTS_DREAM_RUNAWAY_FLUID_HPP*/
