0dc78a106e78393c0a8877d6b1e98ac68f3b6ca1
Ola Embreus <embreus@chalmers.se>
2020-06-11 12:03:22 +0200

    WIP on new transientterm

---
+++ fvm/Equation/LinearTransientTerm.cpp
@@ -0,0 +1,79 @@
+/**
+ * Implementation of an Euler backward transient term multiplied
+ * by an arbitrary grid-dependent weight function. 
+ * Evaluation of weights must be implemented in derived classes. 
+ */
+
+#include <iostream>
+#include "FVM/config.h"
+#include "FVM/Matrix.hpp"
+#include "FVM/Equation/LinearTransientTerm.hpp"
+#include "FVM/Grid/Grid.hpp"
+
+using namespace DREAM::FVM;
+
+
+/**
+ * Constructor.
+ */
+LinearTransientTerm::LinearTransientTerm(Grid *grid, const len_t unknownId)
+        : DiagonalTerm(grid), unknownId(unknownId){}
+
+
+/**
+ * Rebuild the transient term.
+ *
+ * dt: Length of next time step to take.
+ */
+void LinearTransientTerm::Rebuild(const real_t, const real_t dt, UnknownQuantityHandler *uqty) {
+    this->dt = dt;
+    this->xn = uqty->GetUnknownDataPrevious(this->unknownId);
+
+    if(!hasBeenInitialized){
+        InitializeWeights();
+        hasBeenInitialized = true;
+    }
+    if(TermDependsOnUnknowns()) 
+        SetWeights();
+}
+
+
+/**
+ * Set the matrix elements corresponding to this
+ * transient term.
+ *
+ * This term assumes that the linearized matrix equation
+ * to solve is of the form
+ *
+ *   df/dt + Mf = -S
+ *
+ * where M is a linear matrix operator represented by 'mat',
+ * and 'S' is a source term stored in 'rhs'.
+ *
+ * mat: Matrix to set elements of.
+ * rhs: Equation RHS.
+ */
+void LinearTransientTerm::SetMatrixElements(Matrix *mat, real_t *rhs) {
+    const len_t N = grid->GetNCells();
+    for (len_t i = 0; i < N; i++)
+        mat->SetElement(i, i, weights[i]/this->dt, ADD_VALUES);
+
+    if (rhs != nullptr)
+        for (len_t i = 0; i < N; i++)
+            rhs[i] -= weights[i]*this->xn[i] / this->dt;
+}
+
+/**
+ * Set the elements in the function vector 'F' (i.e.
+ * evaluate this term).
+ *
+ * vec: Vector containing value of 'F' on return.
+ * x:   Previous solution (unused).
+ */
+void LinearTransientTerm::SetVectorElements(real_t *vec, const real_t *xnp1) {
+    const len_t N = grid->GetNCells();
+
+    for (len_t i = 0; i < N; i++)
+        vec[i] += weights[i]*(xnp1[i] - xn[i]) / this->dt;
+}
+
---
+++ include/FVM/Equation/LinearTransientTerm.hpp
@@ -0,0 +1,34 @@
+#ifndef _DREAM_FVM_EQUATION_LINEAR_TRANSIENT_TERM_HPP
+#define _DREAM_FVM_EQUATION_LINEAR_TRANSIENT_TERM_HPP
+
+#include "FVM/config.h"
+#include "FVM/Equation/DiagonalTerm.hpp"
+#include "FVM/Grid/Grid.hpp"
+#include "FVM/Matrix.hpp"
+
+
+namespace DREAM::FVM {
+    class LinearTransientTerm : public DiagonalTerm {
+    private:
+        real_t dt;
+
+        // ID of differentiated quantity
+        len_t unknownId;
+        // Differentiated quantity at the previous time step
+        real_t *xn;
+    protected:
+        real_t *weights = nullptr;
+
+        virtual bool TermDependsOnUnknowns() override {return false;}
+        virtual void AddWeightsJacobian(const len_t, const len_t, Matrix*, const real_t*) override {}
+//        virtual void SetWeights() = 0;
+    public:
+        LinearTransientTerm(Grid*, const len_t);
+        
+        virtual void Rebuild(const real_t, const real_t dt, UnknownQuantityHandler *uqty) override;
+        virtual void SetMatrixElements(Matrix*, real_t*) override;
+        virtual void SetVectorElements(real_t*, const real_t*) override;
+    };
+}
+
+#endif/*_DREAM_FVM_EQUATION_LINEAR_TRANSIENT_TERM_HPP*/
