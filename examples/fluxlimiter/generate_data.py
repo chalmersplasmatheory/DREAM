4fd0b69300f1bf99bb66f94fd6084affe57b8e4d
Ola Embreus <embreus@chalmers.se>
2020-07-26 23:54:09 +0200

    tweaked dreicer convergence scan

--- examples/fluxlimiter/generate_data.py
+++ examples/fluxlimiter/generate_data.py
@@ -50,9 +50,9 @@ ds.eqsys.n_i.addIon(name='D', Z=1, iontype=Ions.IONS_PRESCRIBED_FULLY_IONIZED, n
 ds.eqsys.n_re.avalanche = False
 
 # Hot-tail grid settings
-pmax = 2
-np0 = 1000
-nxi0 = 30
+pmax = .3
+np0 = 500
+nxi0 = 40
 ds.hottailgrid.setNxi(nxi0)
 ds.hottailgrid.setNp(np0)
 ds.hottailgrid.setPmax(pmax)
@@ -85,27 +85,23 @@ ds.timestep.setTmax(1e-3)
 ds.timestep.setNt(10)
 
 #############################
-# Set up convergence scan for the three different advection interpolation schemes
+# Set up convergence scan for the different advection interpolation schemes
 
-npvec = [0,90,130,180,250,350,500,800,np0]
-nxivec = [0,3,5,8,14,22,nxi0]
+npvec = [0,20,30,40,50,60,70,80,90,100,110,120,130,150,200,350,np0]
+nxivec = [0,3,5,8,15,25,nxi0]
 def pIndFunc(index: int, settings: DREAMSettings, baseline):
-    #val = baseline + 70*index
     val = npvec[index]
     settings.hottailgrid.setNp(val)
     return settings, val
 
 def xiIndFunc(index: int, settings: DREAMSettings, baseline):
-    #val = baseline + 8*index
     val = nxivec[index]
     settings.hottailgrid.setNxi(val)
     return settings, val
 
 def addPScan(cs: ConvergenceScan):
-#    cs.addScanParameter(name='hottailgrid.pgrid.np',   baselineValue=np0,  f=pIndFunc,  nvalues=8, startindex=-7)
-    cs.addScanParameter(name='hottailgrid.pgrid.np',   baselineValue=np0,  f=pIndFunc,  nvalues=8, startindex=1)
+    cs.addScanParameter(name='hottailgrid.pgrid.np',   baselineValue=np0,  f=pIndFunc,  nvalues=16, startindex=1)
 def addXiScan(cs: ConvergenceScan):
-#    cs.addScanParameter(name='hottailgrid.xigrid.nxi', baselineValue=nxi0, f=xiIndFunc, nvalues=8, startindex=-7)
     cs.addScanParameter(name='hottailgrid.xigrid.nxi', baselineValue=nxi0, f=xiIndFunc, nvalues=6, startindex=1)
 
 ds.eqsys.f_hot.setAdvectionInterpolationMethod(ad_int=FHot.AD_INTERP_CENTRED)
@@ -120,57 +116,70 @@ cs_QUICK   = ConvergenceScan(ds_QUICK, outparams=['other.fluid.runawayRate'])
 addPScan(cs_QUICK)
 addXiScan(cs_QUICK)
 
+ds.eqsys.f_hot.setAdvectionInterpolationMethod(ad_int=FHot.AD_INTERP_UPWIND)
+ds_UPWIND   = DREAMSettings(ds)
+cs_UPWIND   = ConvergenceScan(ds_UPWIND, outparams=['other.fluid.runawayRate'])
+addPScan(cs_UPWIND)
+addXiScan(cs_UPWIND)
+
+ds.eqsys.f_hot.setAdvectionInterpolationMethod(ad_int=FHot.AD_INTERP_UPWIND_2ND_ORDER)
+ds_UPWIND2   = DREAMSettings(ds)
+cs_UPWIND2   = ConvergenceScan(ds_UPWIND2, outparams=['other.fluid.runawayRate'])
+addPScan(cs_UPWIND2)
+addXiScan(cs_UPWIND2)
+
+
+ds.solver.setType(Solver.NONLINEAR)
+ds.solver.setTolerance(1e-2)
 ds.eqsys.f_hot.setAdvectionInterpolationMethod(fluxlimiterdamping=1, ad_int=FHot.AD_INTERP_SMART)
 ds_SMART   = DREAMSettings(ds)
 cs_SMART   = ConvergenceScan(ds_SMART, outparams=['other.fluid.runawayRate'])
 addPScan(cs_SMART)
 addXiScan(cs_SMART)
 
-ds.solver.setType(Solver.NONLINEAR)
-ds.solver.setTolerance(1e-2)
 ds.eqsys.f_hot.setAdvectionInterpolationMethod(fluxlimiterdamping=1, ad_int=FHot.AD_INTERP_MUSCL)
 ds_MUSCL   = DREAMSettings(ds)
 cs_MUSCL   = ConvergenceScan(ds_MUSCL, outparams=['other.fluid.runawayRate'])
 addPScan(cs_MUSCL)
 addXiScan(cs_MUSCL)
 
-'''
-ds.eqsys.f_hot.setAdvectionInterpolationMethod(fluxlimiterdamping=1, ad_int=FHot.AD_INTERP_SMART_PE)
-ds_SMART_PE   = DREAMSettings(ds)
-cs_SMART_PE   = ConvergenceScan(ds_SMART_PE, outparams=['other.fluid.runawayRate'])
-addPScan(cs_SMART_PE)
-addXiScan(cs_SMART_PE)
+ds.eqsys.f_hot.setAdvectionInterpolationMethod(fluxlimiterdamping=1, ad_int=FHot.AD_INTERP_OSPRE)
+ds_OSPRE   = DREAMSettings(ds)
+cs_OSPRE   = ConvergenceScan(ds_OSPRE, outparams=['other.fluid.runawayRate'])
+addPScan(cs_OSPRE)
+addXiScan(cs_OSPRE)
 
-ds.eqsys.f_hot.setAdvectionInterpolationMethod(fluxlimiterdamping=1, ad_int=FHot.AD_INTERP_MUSCL_PE)
-ds_MUSCL_PE   = DREAMSettings(ds)
-cs_MUSCL_PE   = ConvergenceScan(ds_MUSCL_PE, outparams=['other.fluid.runawayRate'])
-addPScan(cs_MUSCL_PE)
-addXiScan(cs_MUSCL_PE)
-'''
-
-
+ds.eqsys.f_hot.setAdvectionInterpolationMethod(fluxlimiterdamping=1, ad_int=FHot.AD_INTERP_TCDF)
+ds_TCDF   = DREAMSettings(ds)
+cs_TCDF   = ConvergenceScan(ds_TCDF, outparams=['other.fluid.runawayRate'])
+addPScan(cs_TCDF)
+addXiScan(cs_TCDF)
 
 ##############################
 # 2. Run convergence scan
 ##############################
-
+'''
 cs_CENTRED.run()
 cs_CENTRED.save('convergence_CENTRED.h5')
 
 cs_QUICK.run()
 cs_QUICK.save('convergence_QUICK.h5')
 
+cs_UPWIND.run()
+cs_UPWIND.save('convergence_UPWIND.h5')
+
+cs_UPWIND2.run()
+cs_UPWIND2.save('convergence_UPWIND2.h5')
+
 cs_SMART.run()
 cs_SMART.save('convergence_SMART.h5')
-
+'''
 cs_MUSCL.run()
 cs_MUSCL.save('convergence_MUSCL.h5')
-'''
-cs_SMART_PE.run()
-cs_SMART_PE.save('convergence_SMART_PE.h5')
 
-cs_MUSCL_PE.run()
-cs_MUSCL_PE.save('convergence_MUSCL_PE.h5')
-'''
+cs_OSPRE.run()
+cs_OSPRE.save('convergence_OSPRE.h5')
 
+cs_TCDF.run()
+cs_TCDF.save('convergence_TCDF.h5')
 
