5bc385c4f45a6bfec35f5dda6a0404c960571a12
Ola Embreus <embreus@chalmers.se>
2020-10-29 21:20:32 +0100

    fixed error in the radial flux trapped-mirroring condition

--- include/FVM/Grid/Grid.hpp
+++ include/FVM/Grid/Grid.hpp
@@ -181,12 +181,15 @@ namespace DREAM::FVM {
          * is such that the distribution should satisfy the equation f(xi0) = f(-xi0),
          * i.e. is in the negative-pitch trapped region. See doc/notes/trappedbc.tex for details.
          */
-        const bool IsNegativePitchTrappedIgnorableCell(const len_t ir, const len_t j) const{
-            return IsTrapped_f2(ir, 0, j+1) && (momentumGrids[ir]->GetP2_f(j+1) <= 100*realeps);
-        }
-        const bool IsNegativePitchTrappedIgnorableRadialFluxCell(const len_t ir, const len_t j) const {
-            return IsTrapped_fr(ir+1,0,j) || IsNegativePitchTrappedIgnorableCell(ir,j); 
-        }
+        const bool IsNegativePitchTrappedIgnorableCell(const len_t ir, const len_t j) const
+        { return IsTrapped_f2(ir, 0, j+1) && (momentumGrids[ir]->GetP2_f(j+1) <= 100*realeps);}
+
+        /**
+         * Returns true if the radial flux into this cell should be added
+         * into the cell containing -xi0 instead, because of trapping.
+         */
+        const bool IsNegativePitchTrappedIgnorableRadialFluxCell(const len_t ir, const len_t j) const 
+        { return (IsTrapped_fr(ir+1,0,j) && momentumGrids[ir]->GetP2_f(j+1) <= 100*realeps) || IsNegativePitchTrappedIgnorableCell(ir,j);}
 
         /**
          * Getters of lower poloidal-angle bounce points
