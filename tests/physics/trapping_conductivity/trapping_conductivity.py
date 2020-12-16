fixed bug in calculation of current density after recent changes to Vp

tests/physics/trapping_conductivity/trapping_conductivity.py
@@ -63,12 +63,14 @@ def gensettings(T, Z=300, EED=1e-6, n=5e19, yMax=5):
     ds.eqsys.j_ohm.setCorrectedConductivity(JOhm.CORRECTED_CONDUCTIVITY_DISABLED)
     ds.eqsys.j_ohm.setConductivityMode(JOhm.CONDUCTIVITY_MODE_SAUTER_COLLISIONLESS)
 
-    # set non-uniform xi grid with points stradding the trapped-passing boundaries
-    Nxi = 16
+    # set non-uniform xi grid with points tightly straddling the trapped-passing boundaries
+    Nxi = 21
     xi_f = np.linspace(-1,1,Nxi)
+
+    boundaryLayerWidth = 1e-4
     for i in range(np.size(xi0Trapped)):
-        xiAdd1 = xi0Trapped[i] + 0.01
-        xiAdd2 = xi0Trapped[i] - 0.01
+        xiAdd1 = xi0Trapped[i] + 0.5*boundaryLayerWidth
+        xiAdd2 = xi0Trapped[i] - 0.5*boundaryLayerWidth
         xi_f = np.append(xi_f, [-xiAdd1, xiAdd1, -xiAdd2, xiAdd2])
     xi_f.sort()
     ds.hottailgrid.setCustomGrid(xi_f=xi_f)
@@ -120,7 +122,7 @@ def runT(T):
     global R0
 
     ds = gensettings(T=T, Z=300)
-#    ds.save('settings_trapping_conductivity.h5')
+    ds.save('settings_trapping_conductivity.h5')
     do = DREAM.runiface(ds, quiet=True)
     jKinetic = do.eqsys.j_ohm[-1,:]
 
@@ -140,7 +142,7 @@ def run(args):
     global NR
 
     # Tolerance to require for agreement with CODE
-    TOLERANCE = 2e-2
+    TOLERANCE = 1e-2
     success = True
     workdir = pathlib.Path(__file__).parent.absolute()
 
@@ -158,7 +160,6 @@ def run(args):
             #continue
             return False
 
-#        print( jKinetic[i,:] / jFluid[i,:])
         # Compare conductivity right away
         Delta = np.amax(np.abs(jKinetic[i,:] / jFluid[i,:] - 1.0))
         print("Delta = {:.3f}%".format(Delta*100))