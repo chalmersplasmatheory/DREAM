f56297036af930a88abfb8bdfcd18838d88c789a
Mathias Hoppe <hoppe@chalmers.se>
2021-03-08 08:53:46 +0100

    WIP implement backtracking for Newton solver
    The algorithm remains to be integrated with the non-linear solver. Also,
    support for limiting step based on quantities becoming negative should
    be moved from the non-linear solver to the NewtonStepAdjuster class.

---
+++ include/DREAM/Solver/NewtonStepAdjuster.hpp
@@ -0,0 +1,26 @@
+#ifndef _DREAM_SOLVER_NONLINEAR_NEWTON_STEP_ADJUSTER_HPP
+#define _DREAM_SOLVER_NONLINEAR_NEWTON_STEP_ADJUSTER_HPP
+
+#include <petsc.h>
+#include <vector>
+#include "FVM/BlockMatrix.hpp"
+#include "FVM/UnknownQuantityHandler.hpp"
+
+namespace DREAM {
+    class NewtonStepAdjuster {
+    protected:
+        std::vector<len_t> nontrivial_unknowns;
+        FVM::UnknownQuantityHandler *unknowns;
+
+    public:
+        NewtonStepAdjuster(
+            std::vector<len_t>& nu, FVM::UnknownQuantityHandler *uqh
+        ) : nontrivial_unknowns(nu), unknowns(uqh) {}
+        virtual ~NewtonStepAdjuster() {}
+
+        virtual real_t Adjust(Vec&, FVM::BlockMatrix*) = 0;
+    };
+}
+
+#endif/*_DREAM_SOLVER_NONLINEAR_NEWTON_STEP_ADJUSTER_HPP*/
+
