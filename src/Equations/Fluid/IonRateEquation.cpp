b06189b7056b32235ad7e375850182ff9d0e625b
Mathias Hoppe <hoppe@chalmers.se>
2021-11-17 15:09:58 +0100

    Remove small elements in IonRateEquation

--- src/Equations/Fluid/IonRateEquation.cpp
+++ src/Equations/Fluid/IonRateEquation.cpp
@@ -192,7 +192,7 @@ bool IonRateEquation::SetCSJacobianBlock(
 
     #define NI(J,V) \
         do {\
-            if (nions[rOffset+ir+(J)*Nr] > 1 && (V) > 1e-40) { \
+            if (nions[rOffset+ir+(J)*Nr] > 1 && std::abs((V)) > 1e-40) { \
                 jac->SetElement(\
                     rOffset+ir, ir, \
                     (V) * nions[rOffset+ir+(J)*Nr] \
@@ -229,10 +229,14 @@ void IonRateEquation::SetCSMatrixElements(
 ) {
     bool setIonization = addFluidIonization || (sm==JACOBIAN&&addFluidJacobian);
     #define NI(J,V) \
-        mat->SetElement(\
-            rOffset+ir, rOffset+ir+(J)*Nr, \
-            (V) \
-        )
+        do { \
+            if (std::abs((V)) > 1e-40) { \
+                mat->SetElement(\
+                    rOffset+ir, rOffset+ir+(J)*Nr, \
+                    (V) \
+                ); \
+            } \
+        } while (false)
     #   include "IonRateEquation.set.cpp"
     #undef NI
 }
@@ -252,7 +256,11 @@ void IonRateEquation::SetCSVectorElements(
 ) {
     bool setIonization = addFluidIonization;
     #define NI(J,V) \
-        vec[rOffset+ir] += (V) * nions[rOffset+ir+(J)*Nr]
+        do { \
+            if (nions[rOffset+ir+(J)*Nr] > 1 && std::abs((V)) > 1e-40) { \
+                vec[rOffset+ir] += (V) * nions[rOffset+ir+(J)*Nr]; \
+            } \
+        } while (false)
     #   include "IonRateEquation.set.cpp"
     #undef NI
 }
