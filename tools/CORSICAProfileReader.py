850554ade1e0aa5cb8ccc84adbb7bd376d8d3011
Oskar Vallhagen <oskar.vallhagen@gmail.com>
2022-10-26 18:49:50 +0200

    Changed name of ITER_Profile_reader and did some cleanups

--- tools/CORSICAProfileReader.py
+++ tools/CORSICAProfileReader.py
@@ -2,63 +2,71 @@
 
 import numpy as np
 
-class ITER_Profile_reader:
+class CORSICAProfileReader:
 
     def __init__(self, filename):
         self.filename = filename
         
-        f = open(self.filename,'r')
-        lines = f.readlines()
-        iLine = 0
-        keep_reading = True
+        self.ncol = None
+        self.N_Rho = None
+        self.Ip = None
         
-        # Go through the file and find basic scalar data 
+        self.load_basics()     
+            
+    def load_basics(self):  
+        # This function goes through the file and finds basic scalar data 
         # (number of columns ncol for the profile datasets, 
         # number of radial gridpoints N_Rho and the plasma current Ip)
-        while keep_reading:
+        
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
+        
+        iLine = 0
+        
+        while iLine<len(lines):
             line = lines[iLine]
-            #print(line)
-            if line.split():
-                if line.split()[0] == 'ncol':
+            line_content = line.split()
+            if line_content:
+                if line_content[0] == 'ncol':
                     self.ncol = int(lines[iLine+1])
                     iLine+=1
-                if line.split()[0] == 'N_Rho':
+                if line_content[0] == 'N_Rho':
                     self.N_Rho = int(lines[iLine+1])
                     iLine+=1
                     
-                if line.split()[0] == 'plasma' and line.split()[1] == 'current':
+                if line_content[0] == 'plasma' and line_content[1] == 'current':
                     self.Ip = float(lines[iLine+1].replace('D','e'))
                     iLine+=1
                             
             
-            iLine+=1        
-            keep_reading = iLine<len(lines)
+            iLine+=1     
         
     def load_profile(self, quantity):
         # This function goes through the file and finds the profile dataset named 'quantity' 
         # in the input file, where 'quantity' should be a string matching one of the 
         # quantity names in the CORSICA file, and returns this profile as a numpy array
         
-        f = open(self.filename,'r')
-        lines = f.readlines()
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
         iLine = 0
-        keep_reading = True
-        while keep_reading:
+        while iLine<len(lines):
             line = lines[iLine]
-            if line.split():
-                if line.split()[0] == quantity:
+            line_content = line.split()
+            if line_content:
+                if line_content[0] == quantity:
                     values = np.zeros(self.N_Rho)
                     iRho = 0
                     while iRho<self.N_Rho:
                         line = lines[iLine+1]
-                        for iCol in range(len(line.split())):
-                            values[iRho] = float(line.split()[iCol].replace('D','e'))
+                        line_content = line.split()
+                        for iCol in range(len(line_content)):
+                            values[iRho] = float(line_content[iCol].replace('D','e'))
                             iRho+=1
                         iLine+=1
                             
             
             iLine+=1        
-            keep_reading = iLine<len(lines)
+
         return values
         
     def get_Ip(self):
