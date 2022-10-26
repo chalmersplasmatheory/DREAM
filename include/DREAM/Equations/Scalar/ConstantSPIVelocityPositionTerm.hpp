8eaa28e3baf50ae8f720b5c64773545cc91b0421
Oskar Vallhagen <oskar.vallhagen@gmail.com>
2022-10-26 17:42:38 +0200

    Separated hpp and cpp file for SPI position term and fixed memory handling of the time delays

--- include/DREAM/Equations/Scalar/ConstantSPIVelocityPositionTerm.hpp
+++ include/DREAM/Equations/Scalar/ConstantSPIVelocityPositionTerm.hpp
@@ -1,9 +1,8 @@
-#include <iostream>
-
 #include "FVM/config.h"
 #include "FVM/Equation/EquationTerm.hpp"
 #include "FVM/Equation/PredeterminedParameter.hpp"
 #include "FVM/Grid/Grid.hpp"
+#include "DREAM/Settings/OptionConstants.hpp"
 
 #ifndef _DREAM_FVM_EQUATION_CONSTANT_SPI_VELOCITY_POSITION_TERM_HPP
 #define _DREAM_FVM_EQUATION_CONSTANT_SPI_VELOCITY_POSITION_TERM_HPP
@@ -21,60 +20,14 @@ namespace DREAM {
         const real_t *t_delay = nullptr;
 
     public:
-        ConstantSPIVelocityPositionTerm(FVM::Grid *g, FVM::UnknownQuantityHandler* u, const real_t *xp,  const real_t *t_delay = nullptr): FVM::PredeterminedParameter(g){
-            id_vp=u->GetUnknownID(OptionConstants::UQTY_V_P);
-            len_t id_Yp=u->GetUnknownID(OptionConstants::UQTY_Y_P);
-            nShard=u->GetUnknown(id_Yp)->NumberOfMultiples();
-            this->currentData = new real_t[3*nShard];
-            this->SetData(xp);
-            if(t_delay != nullptr)
-                this->SetTimeDelay(t_delay);
-        }
-        ~ConstantSPIVelocityPositionTerm(){
-            delete [] currentData;
-            delete [] initial_data;
-        }
+        ConstantSPIVelocityPositionTerm(FVM::Grid *g, FVM::UnknownQuantityHandler* u, const real_t *xp,  const real_t *t_delay = nullptr);
+        ~ConstantSPIVelocityPositionTerm();
 
 
         const real_t *GetData() { return currentData; }
-        void SetData(const real_t* xp_init, bool copy=true){
-            if(copy){
-                real_t *initial_data_xp=new real_t[3*nShard];
-                for(len_t ip=0;ip<nShard;ip++){
-                    initial_data_xp[3*ip]=xp_init[3*ip];
-                    initial_data_xp[3*ip+1]=xp_init[3*ip+1];
-                    initial_data_xp[3*ip+2]=xp_init[3*ip+2];
-                }
-                this->initial_data=initial_data_xp;
-            }else{
-                this->initial_data=xp_init;
-            }
-        }
-        void SetTimeDelay(const real_t *t_delay, bool copy=true){
-            if(copy){
-                real_t *data_tmp=new real_t[nShard];
-                for(len_t ip=0;ip<nShard;ip++){
-                    data_tmp[ip]=t_delay[ip];
-                }
-                this->t_delay=data_tmp;
-            }else{
-                this->t_delay=t_delay;
-            }
-        }
-        virtual void Rebuild(const real_t t, const real_t dt, FVM::UnknownQuantityHandler* u) override{
-            vp=u->GetUnknownData(id_vp);
-            for(len_t ip=0;ip<nShard;ip++){
-                if(t+dt>t_delay[ip]){
-                    currentData[3*ip]=initial_data[3*ip]+vp[3*ip]*(t+dt-t_delay[ip]);
-                    currentData[3*ip+1]=initial_data[3*ip+1]+vp[3*ip+1]*(t+dt-t_delay[ip]);
-                    currentData[3*ip+2]=initial_data[3*ip+2]+vp[3*ip+2]*(t+dt-t_delay[ip]);
-                }else{
-                    currentData[3*ip]=initial_data[3*ip];
-                    currentData[3*ip+1]=initial_data[3*ip+1];
-                    currentData[3*ip+2]=initial_data[3*ip+2];
-                }
-            }
-        }
+        void SetData(const real_t* xp_init, bool copy=true);
+        void SetTimeDelay(const real_t *t_delay, bool copy=true);
+        virtual void Rebuild(const real_t t, const real_t dt, FVM::UnknownQuantityHandler* u) override;
         virtual void SetMatrixElements(FVM::Matrix*, real_t*) override{}
         virtual void SetVectorElements(real_t*, const real_t*) override{}
     };
