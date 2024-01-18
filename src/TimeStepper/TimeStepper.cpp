5fb86044d0e3a38b680693bbe243079a42932a2f
Peter Halldestam <peterhalldestam@outlook.com>
2024-01-18 16:11:22 +0100

    Revert "added alternate way for setting timesteps (ionization based) that makes it compatible with the SPI module"
    This reverts commit 7ad37ffa7d3389a65e4aacc22897044257c51ee9.

--- src/TimeStepper/TimeStepper.cpp
+++ src/TimeStepper/TimeStepper.cpp
@@ -1,166 +1,64 @@
 /**
- * Construct a time stepper object.
+ * Implementation of routines common for all TimeStepper classes.
  */
 
+#include <string>
+#include "DREAM/IO.hpp"
 #include "DREAM/EquationSystem.hpp"
-#include "DREAM/Settings/SimulationGenerator.hpp"
 #include "DREAM/TimeStepper/TimeStepper.hpp"
-#include "DREAM/TimeStepper/TimeStepperConstant.hpp"
-#include "FVM/UnknownQuantityHandler.hpp"
 
 
 using namespace DREAM;
-using namespace std;
-
-
-#define MODULENAME "timestep"
 
 
 /**
- * Define options for the time stepper.
- *
- * s: Settings object to define settings in.
+ * Calls a previously provided Python routine to determine
+ * whether or not time stepping should continue.
  */
-void SimulationGenerator::DefineOptions_TimeStepper(Settings *s) {
-	s->DefineSetting(MODULENAME "/automaticstep", "Step length for the automatic determination of the time step in the ionization time stepper.", (real_t)1e-12);
-    s->DefineSetting(MODULENAME "/checkevery", "Check the error every N'th step (0 = check error after _every_ time step)", (int_t)0);
-    s->DefineSetting(MODULENAME "/constantstep", "Override the adaptive stepper and force a constant time step (DEBUG OPTION)", (bool)false);
-    s->DefineSetting(MODULENAME "/dt", "Length of each time step", (real_t)0.0);
-	s->DefineSetting(MODULENAME "/dtmax", "Maximum allowed time step for the adaptive ionization time stepper.", (real_t)0);
-	s->DefineSetting(MODULENAME "/minsavedt", "Minimum time required to elapse between time steps to save in adaptive ioniz time stepper.", (real_t)0);
-	s->DefineSetting(MODULENAME "/nsavesteps", "Number of time steps to save to output (downsampling)", (int_t)0);
-    s->DefineSetting(MODULENAME "/nt", "Number of time steps to take", (int_t)0);
-	s->DefineSetting(MODULENAME "/safetyfactor", "Safety factor to use when automatically determining the baseline timestep for the adaptive ionization time stepper.", (real_t)50);
-    s->DefineSetting(MODULENAME "/tmax", "Maximum simulation time", (real_t)0.0);
-	s->DefineSetting(MODULENAME "/alpha", "Scaling factor for the ionization time. If not zero, timesteps are updated as: dt = min(dtmax, alpha * t_ioniz)", (real_t)0.0);
-    s->DefineSetting(MODULENAME "/type", "Time step generator type", (int_t)OptionConstants::TIMESTEPPER_TYPE_CONSTANT);
-    s->DefineSetting(MODULENAME "/verbose", "If true, generates excessive output", (bool)false);
-
-    // Tolerance settings for adaptive time stepper
-    DefineToleranceSettings(MODULENAME, s);
+bool TimeStepper::PythonIsTerminate() {
+    if (this->python_terminate_func != nullptr && this->python_caller != nullptr)
+        return this->python_caller(this->python_terminate_func, this->eqsys->GetSimulation());
+    else
+        return false;
 }
 
 /**
- * Construct a TimeStepper object according to the settings.
- *
- * eqsys: Equation system object to assign TimeStepper object to.
- * s:     Settings object specifying how to construct the TimeStepper object.
+ * Check if a given unknown contains negative elements.
  */
-void SimulationGenerator::ConstructTimeStepper(EquationSystem *eqsys, Settings *s) {
-    enum OptionConstants::timestepper_type type = (enum OptionConstants::timestepper_type)s->GetInteger(MODULENAME "/type");
+bool TimeStepper::CheckNegative(const std::string& name) {
+    len_t uqtyid = unknowns->GetUnknownID(name);
 
-    FVM::UnknownQuantityHandler *u = eqsys->GetUnknownHandler();
-    vector<len_t> *nontrivials = eqsys->GetNonTrivialUnknowns();
-    TimeStepper *ts;
-    switch (type) {
-        case OptionConstants::TIMESTEPPER_TYPE_CONSTANT:
-            ts = ConstructTimeStepper_constant(s, u);
-            break;
-
-        case OptionConstants::TIMESTEPPER_TYPE_ADAPTIVE:
-            ts = ConstructTimeStepper_adaptive(s, u, nontrivials);
-            break;
-
-		case OptionConstants::TIMESTEPPER_TYPE_IONIZATION:
-			ts = ConstructTimeStepper_ionization(s, u);
-			break;
-
-        default:
-            throw SettingsException(
-                "Unrecognized time stepper type: %d.", type
-            );
+    if (unknowns->HasUnknown(name)) {
+        FVM::UnknownQuantity *uqty = unknowns->GetUnknown(uqtyid);
+        const real_t *data = uqty->GetData();
+        for (len_t i = 0; i < uqty->NumberOfElements(); i++)
+            if (data[i] < 0)
+                return true;
     }
 
-    eqsys->SetTimeStepper(ts);
-}
-
-
-/**
- * Construct a TimeStepperConstant object according to the
- * provided settings.
- *
- * s: Settings object specifying how to construct the
- *    TimeStepperConstant object.
- */
-TimeStepperConstant *SimulationGenerator::ConstructTimeStepper_constant(Settings *s, FVM::UnknownQuantityHandler *u) {
-    real_t tmax = s->GetReal(MODULENAME "/tmax");
-    real_t dt   = s->GetReal(MODULENAME "/dt", false);
-    int_t nt    = s->GetInteger(MODULENAME "/nt", false);
-    int_t nSaveSteps = s->GetInteger(MODULENAME "/nsavesteps");
-
-    bool dtset  = (dt > 0);
-    bool ntset  = (nt > 0);
-
-    if (dtset && ntset)
-        throw SettingsException(
-            "TimeStepper constant: "
-            "Ambiguous time step specified. Only one of 'dt' and 'nt' "
-            "may be set for the time stepper."
-        );
-    else if (!dtset && !ntset)
-        throw SettingsException(
-            "TimeStepper constant: "
-            "No time step specified. Exactly one of 'dt' and 'nt' "
-            "must be set for the time stepper."
-        );
-
-    // Generate object
-    if (dtset) {
-        s->MarkUsed(MODULENAME "/dt");
-        return new TimeStepperConstant(tmax, dt, u, nSaveSteps);
-    } else {
-        s->MarkUsed(MODULENAME "/nt");
-        return new TimeStepperConstant(tmax, (len_t)nt, u, nSaveSteps);
-    }
+    return false;
 }
 
 /**
- * Construct a TimeStepperAdaptive object according to the
- * provided settings.
+ * This method is called when an exception was thrown while
+ * the next time step was being taken. In the constant time
+ * stepper, we choose to forward this exception and just indicate
+ * to the user that it might be due to the time step being too
+ * short.
  *
- * s: Settings object specifying how to construct the
- *    TimeStepperAdaptive object.
+ * ex: The exception that was caught.
  */
-TimeStepperAdaptive *SimulationGenerator::ConstructTimeStepper_adaptive(
-    Settings *s, FVM::UnknownQuantityHandler *u,
-    vector<len_t> *nontrivials
-) {
-    int_t checkevery = s->GetInteger(MODULENAME "/checkevery");
-    real_t tmax = s->GetReal(MODULENAME "/tmax");
-    real_t dt = s->GetReal(MODULENAME "/dt");
-    bool verbose = s->GetBool(MODULENAME "/verbose");
-    bool conststep = s->GetBool(MODULENAME "/constantstep");
+void TimeStepper::HandleException(FVM::FVMException &ex) {
+    DREAM::IO::PrintError("TimeStepper: Exception caught during time stepping.");
+    DREAM::IO::PrintError("TimeStepper: Perhaps the exception could be avoided by decreasing the time step length?");
 
-    if (dt == 0)
-        dt = 1;
+//    if (CheckNegative(OptionConstants::UQTY_ION_SPECIES))
+//        DREAM::IO::PrintError("TimeStepper: Ion density 'n_i' is negative.");
+    if (CheckNegative(OptionConstants::UQTY_N_COLD))
+        DREAM::IO::PrintError("TimeStepper: Cold electron density 'n_cold' is negative.");
+    if (CheckNegative(OptionConstants::UQTY_T_COLD))
+        DREAM::IO::PrintError("TimeStepper: Cold electron temperature 'T_cold' is negative.");
 
-    ConvergenceChecker *cc = LoadToleranceSettings(
-        MODULENAME, s, u, *nontrivials
-    );
-
-    return new TimeStepperAdaptive(tmax, dt, u, *nontrivials, cc, checkevery, verbose, conststep);
+    throw ex;
 }
 
-/**
- * Construct a TimeStepperIonization object according to the
- * provided settings.
- *
- * s: Settings object specifying how to construct the
- *    TimeStepperIonization object.
- */
-TimeStepperIonization *SimulationGenerator::ConstructTimeStepper_ionization(
-	Settings *s, FVM::UnknownQuantityHandler *u
-) {
-	real_t automaticstep = s->GetReal(MODULENAME "/automaticstep");
-	real_t dt = s->GetReal(MODULENAME "/dt");
-	real_t dtmax = s->GetReal(MODULENAME "/dtmax");
-	real_t minSaveDt = s->GetReal(MODULENAME "/minsavedt");
-	real_t safetyfactor = s->GetReal(MODULENAME "/safetyfactor");
-	real_t tmax = s->GetReal(MODULENAME "/tmax");
-	real_t alpha = s->GetReal(MODULENAME "/alpha");
-
-	if (dt < 0)
-		throw SettingsException("TimeStepper ionization: Initial time step 'dt0' must be non-negative.");
-
-	return new TimeStepperIonization(tmax, dt, dtmax, u, automaticstep, safetyfactor, minSaveDt, alpha);
-}
