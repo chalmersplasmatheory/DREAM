1f95cc4f5f1701c81ee833e2009694bb632641f6
Mathias Hoppe <mhop@kth.se>
2024-02-07 16:10:30 +0100

    Handle case when HDF5 string is 'str'

--- py/DREAM/DataObject.py
+++ py/DREAM/DataObject.py
@@ -118,7 +118,10 @@ class DataObject:
                 return self.data[:].tostring().decode('utf-8')
             elif self.data.dtype == 'object':  # New strings
                 if self.data.shape == ():
-                    return self.data[()].decode()
+                    if type(self.data[()]) == str:
+                        return self.data[()]
+                    else:
+                        return self.data[()].decode()
                 elif type(self.data[:][0]) == str:
                     return self.data[:][0]
                 else:
