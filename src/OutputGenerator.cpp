2caa94540b758005b2b43bf518c9b7cb4cffd961
Ida Ekmark <idaa@ekmark.eu>
2024-07-03 13:42:43 +0200

    Fixed bugs in expensive other quantities

--- src/OutputGenerator.cpp
+++ src/OutputGenerator.cpp
@@ -31,27 +31,35 @@ OutputGenerator::~OutputGenerator() {}
 void OutputGenerator::Save(bool current) {
     // TODO Save settings
 
+    printf("\nGrid\n");
     // Save grids
     this->SaveGrids("grid", current);
-    
+
+    printf("\nEqsys\n");    
     // Save unknowns
     this->SaveUnknowns("eqsys", current);
 
+    printf("\nIonmeta\n");
     // Save ion metadata
     this->SaveIonMetaData("ionmeta");
 
+    printf("\nSettings\n");
     // Save settings
     if (this->savesettings)
         this->SaveSettings("settings");
 
+    printf("\nSolver\n");
     // Save solver statistics
     this->SaveSolverData("solver");
 
+    printf("\nTimings\n");
     // Save timing information
     this->SaveTimings("timings");
 
+    printf("\nOther\n");
     // Save "other" quantities (if any)
     if (oqty->GetNRegistered() > 0 && !current)
         this->SaveOtherQuantities("other");
+    printf("\nDone\n");
 }
 
