b1a730795e2fa09a87d5299b50f570ed6d1d6bc8
Mathias Hoppe <mhop@kth.se>
2024-11-19 17:01:30 +0100

    Separate coefficient construction from 'Rebuild()'

--- src/Equations/Fluid/HyperresistiveDiffusionTerm.cpp
+++ src/Equations/Fluid/HyperresistiveDiffusionTerm.cpp
@@ -30,10 +30,14 @@ HyperresistiveDiffusionTerm::~HyperresistiveDiffusionTerm() {
 /**
  * Build the coefficients of this diffusion term.
  */
-void HyperresistiveDiffusionTerm::Rebuild(const real_t t, const real_t, FVM::UnknownQuantityHandler *){
-    FVM::RadialGrid *rGrid = grid->GetRadialGrid(); 
+void HyperresistiveDiffusionTerm::Rebuild(
+	const real_t t, const real_t, FVM::UnknownQuantityHandler *unknowns
+) {
+    //FVM::RadialGrid *rGrid = grid->GetRadialGrid(); 
     const real_t *Lmbd  = this->EvaluateLambda(t);
+	this->BuildCoefficient(t, unknowns, Lmbd, this->drr);
 
+	/*
     // XXX: here we assume that all radii have the same momentum grids
     const len_t np1 = n1[0], np2 = n2[0];
 
@@ -60,6 +64,45 @@ void HyperresistiveDiffusionTerm::Rebuild(const real_t t, const real_t, FVM::Unk
         for (len_t j = 0; j < np2; j++) 
             for (len_t i = 0; i < np1; i++) 
                 Drr(ir, i, j) += drr;
+    }*/
+}
+
+/**
+ * Set diffusion coefficient, or derivative of, diffusion coefficient.
+ */
+void HyperresistiveDiffusionTerm::BuildCoefficient(
+	const real_t t, FVM::UnknownQuantityHandler*,
+	const real_t *coeff, const real_t **diffusion_coeff
+) {
+    FVM::RadialGrid *rGrid = grid->GetRadialGrid(); 
+
+    // XXX: here we assume that all radii have the same momentum grids
+    const len_t np1 = n1[0], np2 = n2[0];
+
+    // (skip ir=0 since psi_t=0 there, and to avoid 1/psitPrime = 1/0)
+    for (len_t ir = 1; ir < nr+1; ir++) {
+        real_t Bmin = rGrid->GetBmin_f(ir);
+        real_t BdotPhi = rGrid->GetBTorG_f(ir)*rGrid->GetFSA_1OverR2_f(ir);
+        real_t VpVol = rGrid->GetVpVol_f(ir); 
+
+        real_t psitPrime = VpVol*BdotPhi / (2*M_PI);
+
+        // The entire d psi/dt equation is multiplied by 2*pi*psi_t'/VpVol,
+        // so we get an extra factor of 2*pi here, and only one factor
+        // of psi_t' (the factor 1/VpVol from the normalisation is included 
+        // via the DiffusionTerm class). We also add a factor of 1/VpVol 
+        // to the diffusion coefficient to cancel the factor VpVol inside 
+        // the first radial derivative added by the DiffusionTerm. 
+        //
+        // Also, we divide by 'Bmin' since this operator is applied to
+        // 'j_tot / (B/Bmin)'.
+        real_t drr = 
+            2*M_PI*rGrid->GetToroidalFlux_f(ir)*coeff[ir] / (VpVol*psitPrime*Bmin);
+
+        for (len_t j = 0; j < np2; j++) 
+            for (len_t i = 0; i < np1; i++) 
+                //Drr(ir, i, j) += drr;
+				diffusion_coeff[ir][j*np1 + i] += drr;
     }
 }
 
