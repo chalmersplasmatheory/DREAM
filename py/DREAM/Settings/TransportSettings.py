7a14122dbec70ec8554eaec10f0dd1964fd121c1
Mathias Hoppe <mhop@kth.se>
2025-04-27 14:32:12 +0200

    Fix issue #425

--- py/DREAM/Settings/TransportSettings.py
+++ py/DREAM/Settings/TransportSettings.py
@@ -664,7 +664,7 @@ class TransportSettings:
             if g('_interp3d') not in [INTERP3D_LINEAR, INTERP3D_NEAREST, INTERP3D_LOGARITHMIC]:
                 raise TransportException("{}: Invalid value assigned to interp3d.".format(coeff))
 
-            if coeff+'v' in self.__dict__:
+            if (coeff+v) in self.__dict__:
                 if g('_interp1d') not in [INTERP1D_LINEAR, INTERP1D_NEAREST, INTERP1D_LOGARITHMIC]:
                     raise TransportException("{}: Invalid value assigned to interp1d.".format(coeff))
 
