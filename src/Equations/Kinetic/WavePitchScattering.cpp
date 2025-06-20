1299c8fbc3977fd61ab5c887ee172147270b207b
Tijs Wijkamp <t.a.wijkamp@tue.nl>
2025-06-20 22:50:24 +0200

    Change to WavePitchScattering.cpp

--- src/Equations/Kinetic/WavePitchScattering.cpp
+++ src/Equations/Kinetic/WavePitchScattering.cpp
@@ -1,7 +1,6 @@
 /**
  * This term implements the pitch-angle diffusion resulting when electrons
- * interact with the magnetic ripple in a tokamak. The diffusion coefficient
- * was first derived in [Kurzan et al, PRL 75 4626 (1995)].
+ * interact with waves injected into a tokamak.
  */
 
 #include <algorithm>
@@ -29,7 +28,8 @@ using namespace std;
 WavePitchScattering::WavePitchScattering(
     FVM::Grid *grid, enum OptionConstants::eqterm_wave_mode mode,
     enum OptionConstants::momentumgrid_type mgtype,
-    DREAM::MultiInterpolator1D *ppar_res, DREAM::MultiInterpolator1D *Delta_ppar_res, DREAM::MultiInterpolator1D *Dxx_int
+    DREAM::MultiInterpolator1D *ppar_res, DREAM::MultiInterpolator1D *Delta_ppar_res,
+    DREAM::MultiInterpolator1D *Dxx_int
 ) : WavePitchScattering(
     grid, mode, mgtype, ppar_res, Delta_ppar_res, Dxx_int
 ) {
@@ -39,7 +39,8 @@ WavePitchScattering::WavePitchScattering(
 WavePitchScattering::WavePitchScattering(
     FVM::Grid *grid, enum OptionConstants::eqterm_wave_mode mode,
     enum OptionConstants::momentumgrid_type mgtype,
-    DREAM::MultiInterpolator1D *ppar_res, DREAM::MultiInterpolator1D *Delta_ppar_res, DREAM::MultiInterpolator1D *Dxx_int
+    DREAM::MultiInterpolator1D *ppar_res, DREAM::MultiInterpolator1D *Delta_ppar_res,
+    DREAM::MultiInterpolator1D *Dxx_int
 ) : DiffusionTerm(grid), mode(mode), ppar_res(ppar_res), Delta_ppar_res(Delta_ppar_res), Dxx_int(Dxx_int)
 {
     
@@ -53,19 +54,19 @@ WavePitchScattering::WavePitchScattering(
 }
 
 /**
- * Allocate memory used by this term.
- */
-void WavePitchScattering::Allocate() {
-    // nothing to be allocated
-}
-
-/**
  * Destructor.
  */
 WavePitchScattering::~WavePitchScattering() {
     delete this->ppar_res;
     delete this->Delta_ppar_res;
     delete this->Dxx_int;
+}
+
+/**
+ * Allocate memory used by this term.
+ */
+void WavePitchScattering::Allocate() {
+    // nothing to be allocated
 }
 
 /**
@@ -86,9 +87,9 @@ void WavePitchScattering::Rebuild(const real_t t, const real_t, FVM::UnknownQuan
     const len_t nr = this->grid->GetNr();
     
     // evaluate wave quantities at this time
-    const real_t *ppar_res_t =this->ppar_res->Eval(1, t);
-    const real_t *Delta_ppar_res_t =this->Delta_ppar_res->Eval(1, t);
-    const real_t *Dxx_int_t =this->Dxx_int->Eval(1, t);
+    const real_t *ppar_res_t = this->ppar_res->Eval(1, t);
+    const real_t *Delta_ppar_res_t = this->Delta_ppar_res->Eval(1, t);
+    const real_t *Dxx_int_t = this->Dxx_int->Eval(1, t);
     
     // loop over radial coordinate
     for (len_t ir = 0; ir < nr; ir++) {
@@ -97,25 +98,26 @@ void WavePitchScattering::Rebuild(const real_t t, const real_t, FVM::UnknownQuan
         const len_t nxi   = mg->GetNp2();
         const real_t *p   = mg->GetP1();
         const real_t *xi0 = mg->GetP2_f();
+        
+        // get diffusion strength at this radius
+        real_t Dxx_int0 = Dxx_int_t[ir];
             
         // loop over pitch angle grid
         for (len_t j_f = 0; j_f < nxi+1; j_f++){
+            // calculate resonant momentum and total diffusion strength at this pitch angle
             const real_t absxi = min(fabs(xi0[j_f]),1.0); // handles precision errors in fabs
+            real_t p_res0 = ppar_res_t[ir] / absxi;
+            real_t Delta_p_res0 = Delta_ppar_res_t[ir] / absxi;
             
             // loop over momentum grid
             for (len_t i = 0; i < np; i++) {
 
                 if (mode == OptionConstants::EQTERM_WAVE_MODE_GAUSSIAN){
                     if (absxi != 0) {
-                        // calculate resonant momentum and total diffusion strength at this pitch angle
-                        real_t p_res0 = ppar_res_t[ir] / absxi;
-                        real_t Delta_p_res0 = Delta_ppar_res_t[ir] / absxi;
-                        real_t Dxx_int0 = Dxx_int_t[ir];
-                        
                         // add to diffusion term
                         const real_t exp_nom = (p[i]-p_res0)*(p[i]-p_res0);
                         const real_t exp_denom = 2*Delta_p_res0*Delta_p_res0;
-                        D22(ir, i, j_f) += Dxx_int0 * exp(-exp_nom/exp_denom);
+                        D22(ir, i, j_f) += Dxx_int0 * exp(-exp_nom/exp_denom) / sqrt(exp_denom*M_PI);
                     }
                 }
             }
