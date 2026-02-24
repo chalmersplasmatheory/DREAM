2e5a71c40f1b1a3557c2694c01e01ec9084ce295
Ola Embreus <ola.embreus@gmail.com>
2026-02-24 17:10:23 +0100

    fix copy constructor on distribution outputs for arithmetics

--- py/DREAM/Output/RunawayElectronDistributionFunction.py
+++ py/DREAM/Output/RunawayElectronDistributionFunction.py
@@ -17,3 +17,29 @@ class RunawayElectronDistributionFunction(DistributionFunction):
         Returns the TeX-compatible name of this quantity.
         """
         return r'$f_{\rm RE}$'
+
+
+    def new_like(self, name=None, data=None, grid=None, output=None, attr=None):
+        """
+        Creates a new object of the same type where the provided quantities replace
+        those of self.
+
+        Unlike KineticQuantity from which this class inherits from,
+        the constructor wants no momentumgrid. Therefore overriding that behavior here.
+        """
+        if name is None:
+            name = self.name
+        if data is None:
+            data = self.data
+        if grid is None:
+            grid = self.grid
+        if output is None:
+            output = self.output
+        if attr is None:
+            attr = {}
+            if hasattr(self, "description"):
+                attr["description"] = self.description
+            if hasattr(self, "description_eqn"):
+                attr["equation"] = self.description_eqn
+
+        return type(self)(name=name, data=data, grid=grid, output=output, attr=attr)
