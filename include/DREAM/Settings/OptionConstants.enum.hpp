ba01dc35d086d246ffabe5b6da666ac331b6529b
IdaEkmark <idaa@ekmark.eu>
2026-07-22 17:45:39 +0200

    Wrong order originally

--- include/DREAM/Settings/OptionConstants.enum.hpp
+++ include/DREAM/Settings/OptionConstants.enum.hpp
@@ -443,9 +443,9 @@ enum eqterm_bootstrap_mode {
     EQTERM_BOOTSTRAP_MODE_REDL_STELLARATOR = 3,
 };
 
-enum eqterm_bootstrap_init_mode {
-    EQTERM_BOOTSTRAP_INIT_MODE_OHMIC = 1, 
-    EQTERM_BOOTSTRAP_INIT_MODE_TOTAL = 2
+enum eqterm_bootstrap_init_mode { 
+    EQTERM_BOOTSTRAP_INIT_MODE_TOTAL = 1,
+    EQTERM_BOOTSTRAP_INIT_MODE_OHMIC = 2
 };
 
 enum eqterm_lcfs_loss_mode {                        // Loss term
