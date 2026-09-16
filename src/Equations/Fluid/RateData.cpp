d645aba0541402619e426915125a20eda0d6a810
ekmanl <linn.astrid.ekman@gmail.com>
2026-09-16 14:58:41 +0200

    Need to know the input (Ti or T_cold) för the interpolation

--- src/Equations/Fluid/RateData.cpp
+++ src/Equations/Fluid/RateData.cpp
@@ -14,12 +14,12 @@ static const MolecularReactionSpecies Ar_Arp_charge_exchange_products[] = {
 
 static const MolecularReactionSpecies Ar_Ar2p_charge_exchange_reactants[] = {
     {"Ar", 0, 1},
-    {"Ar2", 1, 1},
+    {"Ar", 2, 1},
 };
 
 static const MolecularReactionSpecies Ar_Ar2p_charge_exchange_products[] = {
-    {"Ar", 1, 1},
-    {"Ar2", 0, 1},
+    {"Ar", 2, 1},
+    {"Ar", 0, 1},
 };
 
 static const MolecularReactionSpecies D_Dp_charge_exchange_reactants[] = {
@@ -99,7 +99,7 @@ static const MolecularReactionSpecies D_2_dissociation_reactants[] = {
 
 static const MolecularReactionSpecies D_2_dissociation_products[] = {
     {"e", -1, 1},
-    {"D", 1, 2},
+    {"D", 0, 2},
 };
 
 static const MolecularReactionSpecies D_2_ionization_reactants[] = {
@@ -108,8 +108,8 @@ static const MolecularReactionSpecies D_2_ionization_reactants[] = {
 };
 
 static const MolecularReactionSpecies D_2_ionization_products[] = {
-    {"e", -1, 1},
-    {"D2", 0, 1},
+    {"e", -1, 2},
+    {"D2", 1, 1},
 };
 
 static const MolecularReactionSpecies D_2_dissociation_2_reactants[] = {
@@ -232,14 +232,14 @@ static const MolecularReactionSpecies D_2p_recombination_products[] = {
     {"D", 0, 1},
 };
 
-static const MolecularReactionSpecies D_2p_recombination_2_reactants[] = {
+static const MolecularReactionSpecies D_2p_dissociation_reactants[] = {
     {"e", -1, 1},
     {"D2", 1, 1},
 };
 
-static const MolecularReactionSpecies D_2p_recombination_2_products[] = {
+static const MolecularReactionSpecies D_2p_dissociation_products[] = {
     {"D", 0, 1},
-    {"D", 0, 1},
+    {"D", 1, 1},
 };
 
 static const MolecularReactionSpecies D_3p_dissociation_reactants[] = {
@@ -338,7 +338,7 @@ static const MolecularReactionSpecies HeDp_dissociation_reactants[] = {
 
 static const MolecularReactionSpecies HeDp_dissociation_products[] = {
     {"He", 0, 1},
-    {"D", 1, 1},
+    {"D", 0, 1},
 };
 
 static const MolecularReactionSpecies He_D_2p_dissociation_reactants[] = {
@@ -373,7 +373,7 @@ static const MolecularReactionSpecies D_2_HeDp_dissociation_products[] = {
 
 static const MolecularReactionSpecies Hep_D_2_charge_exchange_reactants[] = {
     {"He", 1, 1},
-    {"D2", 1, 1},
+    {"D2", 0, 1},
 };
 
 static const MolecularReactionSpecies Hep_D_2_charge_exchange_products[] = {
@@ -386,229 +386,305 @@ const MolecularReactionDefinition molecularReactionDefinitions[] = {
         "Ar_Ar+_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE_RESONANT,
         2, Ar_Arp_charge_exchange_reactants,
-        2, Ar_Arp_charge_exchange_products
+        2, Ar_Arp_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "Ar", 1, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar_Ar2+_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE_RESONANT,
         2, Ar_Ar2p_charge_exchange_reactants,
-        2, Ar_Ar2p_charge_exchange_products
+        2, Ar_Ar2p_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "Ar", 2, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_D+_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE_RESONANT,
         2, D_Dp_charge_exchange_reactants,
-        2, D_Dp_charge_exchange_products
+        2, D_Dp_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "D", 1, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_2_D_2+_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE_RESONANT,
         2, D_2_D_2p_charge_exchange_reactants,
-        2, D_2_D_2p_charge_exchange_products
+        2, D_2_D_2p_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "D2", 1, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_2+_D_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, D_2p_D_charge_exchange_reactants,
-        2, D_2p_D_charge_exchange_products
+        2, D_2p_D_charge_exchange_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar+_D_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Arp_D_charge_exchange_reactants,
-        2, Arp_D_charge_exchange_products
+        2, Arp_D_charge_exchange_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "Ar2+_D_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Ar2p_D_charge_exchange_reactants,
-        2, Ar2p_D_charge_exchange_products
+        2, Ar2p_D_charge_exchange_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "Ar3+_D_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Ar3p_D_charge_exchange_reactants,
-        2, Ar3p_D_charge_exchange_products
+        2, Ar3p_D_charge_exchange_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "Ar4+_D_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Ar4p_D_charge_exchange_reactants,
-        2, Ar4p_D_charge_exchange_products
+        2, Ar4p_D_charge_exchange_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "D_2_dissociation",
         MolecularReactionProcess::DISSOCIATION,
         2, D_2_dissociation_reactants,
-        2, D_2_dissociation_products
+        2, D_2_dissociation_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "D_2_ionization",
         MolecularReactionProcess::IONIZATION,
         2, D_2_ionization_reactants,
-        2, D_2_ionization_products
+        2, D_2_ionization_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "D_2_dissociation_2",
         MolecularReactionProcess::DISSOCIATION,
         2, D_2_dissociation_2_reactants,
-        2, D_2_dissociation_2_products
+        2, D_2_dissociation_2_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "D+_D_2_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Dp_D_2_charge_exchange_reactants,
-        2, Dp_D_2_charge_exchange_products
+        2, Dp_D_2_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "D", 1, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_2+_D_2_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, D_2p_D_2_dissociation_reactants,
-        2, D_2p_D_2_dissociation_products
+        2, D_2p_D_2_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar+_D_2_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Arp_D_2_charge_exchange_reactants,
-        2, Arp_D_2_charge_exchange_products
+        2, Arp_D_2_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "D2", 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar+_D_2_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, Arp_D_2_dissociation_reactants,
-        2, Arp_D_2_dissociation_products
+        2, Arp_D_2_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar2+_D_2_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Ar2p_D_2_charge_exchange_reactants,
-        2, Ar2p_D_2_charge_exchange_products
+        2, Ar2p_D_2_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "D2", 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar3+_D_2_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Ar3p_D_2_charge_exchange_reactants,
-        2, Ar3p_D_2_charge_exchange_products
+        2, Ar3p_D_2_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "D2", 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar4+_D_2_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Ar4p_D_2_charge_exchange_reactants,
-        2, Ar4p_D_2_charge_exchange_products
+        2, Ar4p_D_2_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "D2", 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "ArD+_D_2_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, ArDp_D_2_dissociation_reactants,
-        2, ArDp_D_2_dissociation_products
+        2, ArDp_D_2_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_2+_Ar_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, D_2p_Ar_dissociation_reactants,
-        2, D_2p_Ar_dissociation_products
+        2, D_2p_Ar_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_3+_Ar_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, D_3p_Ar_dissociation_reactants,
-        2, D_3p_Ar_dissociation_products
+        2, D_3p_Ar_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_2+_recombination",
         MolecularReactionProcess::RECOMBINATION,
         2, D_2p_recombination_reactants,
-        2, D_2p_recombination_products
+        2, D_2p_recombination_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
-        "D_2+_recombination_2",
-        MolecularReactionProcess::RECOMBINATION,
-        2, D_2p_recombination_2_reactants,
-        2, D_2p_recombination_2_products
+        "D_2+_dissociation",
+        MolecularReactionProcess::DISSOCIATION,
+        2, D_2p_dissociation_reactants,
+        2, D_2p_dissociation_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0}
     },
     {
         "D_3+_dissociation",
         MolecularReactionProcess::DISSOCIATION,
         2, D_3p_dissociation_reactants,
-        2, D_3p_dissociation_products
+        2, D_3p_dissociation_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_3+_dissociation_2",
         MolecularReactionProcess::DISSOCIATION,
         2, D_3p_dissociation_2_reactants,
-        1, D_3p_dissociation_2_products
+        1, D_3p_dissociation_2_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_2+Ar2+_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, D_2pAr2p_dissociation_reactants,
-        2, D_2pAr2p_dissociation_products
+        2, D_2pAr2p_dissociation_products,
+        {MolecularInputKind::SPECIES, "D2", 1, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "ArD+_dissociation",
         MolecularReactionProcess::DISSOCIATION,
         2, ArDp_dissociation_reactants,
-        2, ArDp_dissociation_products
+        2, ArDp_dissociation_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "ArD+_dissociation_2",
         MolecularReactionProcess::DISSOCIATION,
         2, ArDp_dissociation_2_reactants,
-        2, ArDp_dissociation_2_products
+        2, ArDp_dissociation_2_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "ArD+_dissociation_3",
         MolecularReactionProcess::DISSOCIATION,
         2, ArDp_dissociation_3_reactants,
-        2, ArDp_dissociation_3_products
+        2, ArDp_dissociation_3_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "He+_He_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE_RESONANT,
         2, Hep_He_charge_exchange_reactants,
-        2, Hep_He_charge_exchange_products
+        2, Hep_He_charge_exchange_products,
+        {MolecularInputKind::RELATIVE, "He", 0, "He", 1},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_He+_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, D_Hep_charge_exchange_reactants,
-        2, D_Hep_charge_exchange_products
+        2, D_Hep_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "He", 1, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "Ar_He+_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Ar_Hep_charge_exchange_reactants,
-        2, Ar_Hep_charge_exchange_products
+        2, Ar_Hep_charge_exchange_products,
+        {MolecularInputKind::SPECIES, "He", 1, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "HeD+_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, HeDp_dissociation_reactants,
-        2, HeDp_dissociation_products
+        2, HeDp_dissociation_products,
+        {MolecularInputKind::ELECTRON, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "He_D_2+_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, He_D_2p_dissociation_reactants,
-        2, He_D_2p_dissociation_products
+        2, He_D_2p_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_HeD+_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, D_HeDp_dissociation_reactants,
-        2, D_HeDp_dissociation_products
+        2, D_HeDp_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "D_2_HeD+_dissociation",
         MolecularReactionProcess::DISSOCIATION_MULTIPLE,
         2, D_2_HeDp_dissociation_reactants,
-        2, D_2_HeDp_dissociation_products
+        2, D_2_HeDp_dissociation_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
     {
         "He+_D_2_charge_exchange",
         MolecularReactionProcess::CHARGE_EXCHANGE,
         2, Hep_D_2_charge_exchange_reactants,
-        2, Hep_D_2_charge_exchange_products
+        2, Hep_D_2_charge_exchange_products,
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0},
+        {MolecularInputKind::NONE, nullptr, 0, nullptr, 0}
     },
 };
 
